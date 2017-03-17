b7d5789a8c671546d120ac3e6727564e1cdeb1a4
Hari KT <kthari85@gmail.com>
2017-03-17 22:11:28 +0530

    Remove factories. Use aura/di

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -1,38 +1,17 @@
 <?php
 namespace Hkt\Psr7AssetExample;
 
-use Hkt\Psr7AssetExample\Middleware\Welcome;
-use Hkt\Psr7AssetExample\Container\WelcomeFactory;
-use Hkt\Psr7AssetExample\Middleware\Asset;
-use Hkt\Psr7AssetExample\Container\AssetFactory;
-
 class ConfigProvider
 {
     public function __invoke()
     {
         return [
-            'dependencies' => $this->getDependencies(),
             'templates' => [
                 'paths' => [
                     'hkt-psr7-asset-example' => [
                         dirname(__DIR__) . '/templates'
                     ],
                 ],
-            ],
-            'middleware_pipeline' => [
-                [
-                    'middleware' => Asset::class,
-                ],
-            ]
-        ];
-    }
-
-    public function getDependencies()
-    {
-        return [
-            'factories' => [
-                Welcome::class => WelcomeFactory::class,
-                Asset::class => AssetFactory::class,
             ],
         ];
     }
---
+++ src/Container/Common.php
@@ -0,0 +1,24 @@
+<?php
+namespace Hkt\Psr7Asset;
+
+use Aura\Di\Container;
+use Aura\Di\ContainerConfigInterface;
+
+class Common implements ContainerConfigInterface
+{
+    public function define(Container $di)
+    {
+        $di->set('Hkt\Psr7AssetExample\Middleware\Welcome', $di->lazyNew('Hkt\Psr7AssetExample\Middleware\Welcome'));
+
+        $di->params['Hkt\Psr7AssetExample\Middleware\Welcome'] = array(
+            'template' => $di->lazyGet('Zend\Expressive\Template\TemplateRendererInterface'),
+            'responseFactory' => $di->lazyGet('Interop\Http\Factory\ResponseFactoryInterface'),
+        );
+    }
+
+    public function modify(Container $di)
+    {
+        $assetLocator = $di->get('Hkt\Psr7Asset\AssetLocator');
+        $assetLocator->set('hkt/psr7-asset-example', dirname(dirname(__DIR__)) . '/public');
+    }
+}
